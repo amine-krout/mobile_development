10f6acec8de8d3d9f83c91484d555b23dddfcc40
amine.krout <mohamedamine.krout@supcom.tn>
2021-12-06 10:33:03 +0100

    re-upload-2

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,14 @@ import 'package:la_corda_car_rental/register_page.dart';
 import 'package:la_corda_car_rental/register_your_car.dart';
 import 'package:la_corda_car_rental/home_page.dart';
 import 'package:la_corda_car_rental/my_ride.dart';
+import 'package:la_corda_car_rental/start_page.dart';
 //import 'package:la_corda_car_rental/widget/home_page.dart';
 // import 'package:la_corda_car_rental/login_page.dart';
 // import 'package:la_corda_car_rental/start_page.dart';
 // import 'package:la_corda_car_rental/widget/home_page.dart';
 
 void main() {
-  runApp(const My_Ride());
+  runApp(const Book_Ride());
 }
+
+//modification 2
